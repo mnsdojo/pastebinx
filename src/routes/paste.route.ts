744d494eadb1522952414ff995df3bc2441f1231
pablo <pablo@pablos-MacBook-Air.local>
2026-01-07 22:50:19 +0530

    added zod schema validation for paste controller

--- src/routes/paste.route.ts
+++ src/routes/paste.route.ts
@@ -1,11 +1,33 @@
 import { Router } from "express";
 import { optionalAuth, requireAuth } from "../middlewares/auth.middleware";
 import { PasteController } from "../controllers/paste.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { createPasteSchema, getPasteSchema, updatePasteSchema } from "../schemas/paste.schema";
 
 const router = Router();
-router.post("/", optionalAuth, PasteController.createPaste);
-router.get("/:id", optionalAuth, PasteController.getPaste);
-router.get("/user/me", requireAuth, PasteController.getMyPastes);
-router.put("/:id", requireAuth, PasteController.updatePaste);
+
+router.get(
+  "/:id",
+  validate(getPasteSchema),
+  optionalAuth,
+  PasteController.getPaste
+);
+
+router.post(
+  "/",
+  validate(createPasteSchema),
+  optionalAuth,
+  PasteController.createPaste
+);
+
+router.get("/me", requireAuth, PasteController.getMyPastes);
+
+router.put(
+  "/:id",
+  validate(updatePasteSchema),
+  requireAuth,
+  PasteController.updatePaste
+);
+
 router.delete("/:id", requireAuth, PasteController.deletePaste);
 export default router;
---
+++ src/schemas/paste.schema.ts
@@ -0,0 +1,55 @@
+import { PasteVisibility } from "@prisma/client";
+import { z } from "../lib/openapi";
+
+export const pasteBaseSchema = {
+  title: z.string().max(200).optional(),
+
+  content: z
+    .string()
+    .min(1, "Content is required")
+    .max(100_000, "Paste is too large"),
+
+  visibility: z.nativeEnum(PasteVisibility).optional(),
+
+  expiresAt: z.iso
+    .datetime()
+    .optional()
+    .nullable()
+    .transform((v) => (v ? new Date(v) : null)),
+};
+
+export const updatePasteSchema = z.object({
+  params: z.object({
+    id: z.string().uuid("Invalid paste id"),
+  }),
+  body: z
+    .object({
+      title: pasteBaseSchema.title,
+      content: pasteBaseSchema.content.optional(),
+      visibility: pasteBaseSchema.visibility,
+      expiresAt: pasteBaseSchema.expiresAt,
+    })
+    .refine(
+      (data) => Object.keys(data).length > 0,
+      "At least one field must be provided"
+    ),
+});
+
+export const createPasteSchema = z.object({
+  body: z.object(pasteBaseSchema),
+});
+
+export const getPasteSchema = z.object({
+  params: z.object({
+    id: z.string().uuid("Invalid paste id"),
+  }),
+});
+
+/**
+ * List pastes by user
+ */
+export const getUserPastesSchema = z.object({
+  params: z.object({
+    userId: z.string().uuid("Invalid user id"),
+  }),
+});
